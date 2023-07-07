c7c3ede8706f196d7ef0c4866e9fb3bdb769152a
PREMEDIA\niklas.schmid <niklas.schmid@premedia.at>
2023-07-07 08:43:38 +0200

    cleanup

---
+++ Premedia.Applications.Imaging.Dashboard.Application/Services/UserApplicationService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Premedia.Applications.Imaging.Dashboard.Application.Contracts;
+using Premedia.Applications.Imaging.Dashboard.Application.ReadModels;
+using Premedia.Applications.Imaging.Dashboard.Persistence;
+using Premedia.Applications.Imaging.Dashboard.Persistence.Contracts;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premedia.Applications.Imaging.Dashboard.Application.Services
+{
+    public class UserApplicationService : BaseApplicationService, IUserApplicationService
+    {
+        private readonly ImagingDashboardDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public UserApplicationService(IUnitOfWork unitOfWork, ImagingDashboardDbContext dbContext, IMapper mapper) : base(unitOfWork)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<ActionResult<List<UserReadModel>>> GetUser()
+        {
+            var user = await _unitOfWork.UserRepository.GetMultipleAsync();
+            return _mapper.Map<List<UserReadModel>>(user);
+        }
+    }
+}
+
---
+++ Premedia.Applications.Imaging.Dashboard.Web/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Premedia.Applications.Imaging.Dashboard.Application.Contracts;
+using Premedia.Applications.Imaging.Dashboard.Application.ReadModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Premedia.Applications.Imaging.Dashboard.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserApplicationService _userApplicationService;
+        public UserController(IUserApplicationService userApplicationService)
+        {
+            _userApplicationService = userApplicationService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserReadModel>>> GetUser()
+        {
+            return await _userApplicationService.GetUser();
+        }
+    }
+}
