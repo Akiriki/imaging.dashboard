94201a49552697ae4c8d2087b2bbd103bbd48e29
PREMEDIA\niklas.schmid <niklas.schmid@premedia.at>
2023-07-20 13:43:03 +0200

    Problem mit Filepath-attributen zu GetTransferredJobFiles hinzufügen behoben

---
+++ Premedia.Applications.Imaging.Dashboard.Application/Mappings/FilePathMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Premedia.Applications.Imaging.Dashboard.Application.ReadModels;
+using Premedia.Applications.Imaging.Dashboard.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premedia.Applications.Imaging.Dashboard.Application.Mappings
+{
+    public class FilePathMapping : Profile
+    {
+        public FilePathMapping() {
+            CreateMap<FilePathReadModel, FilePath>().ReverseMap();
+        }
+    }
+}
