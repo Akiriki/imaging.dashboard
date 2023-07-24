f70c352997399255f977ddfdf34334cf640a307d
PREMEDIA\niklas.schmid <niklas.schmid@premedia.at>
2023-07-24 16:26:33 +0200

    list of easyjobs

--- Premedia.Applications.Imaging.Dashboard.Web/ClientApp/src/app/modules/base/administration/projects/projects.component.ts
+++ Premedia.Applications.Imaging.Dashboard.Web/ClientApp/src/app/modules/base/administration/projects/projects.component.ts
@@ -1,23 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { getInstance } from '@syncfusion/ej2-base';
+import { ListBox } from '@syncfusion/ej2-dropdowns';
 import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
 import { AdministrationService, ProjectDeclaration } from 'src/app/services/administration.service';
 import { DestroySubscriptionsComponent } from 'src/app/shared/destroy-subscriptions/destroy-subscriptions.component';
+import { ListBoxComponent } from '@syncfusion/ej2-angular-dropdowns';
 
 @Component({
-  selector: 'app-projects',
+  selector: 'app-container',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent extends DestroySubscriptionsComponent{
-  projectDeclarationList : ProjectDeclaration[] = [];
-  pageSettings: PageSettingsModel;
+  public data: { [key: string]: Object }[] = [];
+  public newInputValue: string = '';
 
-  constructor(public administrationService : AdministrationService) {
-    super();
-    this.setNewSubscription = administrationService.projectDeclarationContent.subscribe((projectDeclarationContent) => {
-      this.projectDeclarationList = projectDeclarationContent
-    })
-    this.pageSettings = {pageSize : administrationService.pageSettings.pageSize}
-    administrationService.loadProjectDeclaration();
+  //@ViewChild('listbox') listbox!: ListBoxComponent;
+
+  btnclick() {
+    if (this.newInputValue.trim() !== '') {
+      const newItems = this.newInputValue
+        .split(',') // Split input into individual items using commas as the separator
+        .map(item => item.trim()) // Trim whitespace from each item
+        .filter(item => item !== ''); // Remove empty items
+
+      this.data.push(...newItems.map(text => ({ text })));
+      this.newInputValue = '';
+    }
+   /*if(this.listbox){
+    let addItem: { [key: string]: Object }[] = [
+      { text: this.newInputValue}
+    ];
+      this.listbox.addItems(addItem);
+   }
+   console.log(this.newInputValue);
+   this.newInputValue = '';
+   */
   }
 }
+
