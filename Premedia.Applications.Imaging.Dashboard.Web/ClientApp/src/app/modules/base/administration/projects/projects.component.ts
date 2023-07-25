6af9842bf15bb800767b47ea037174360785ac46
PREMEDIA\niklas.schmid <niklas.schmid@premedia.at>
2023-07-25 10:03:43 +0200

    added textarea where you can eneter new Jobs

--- Premedia.Applications.Imaging.Dashboard.Web/ClientApp/src/app/modules/base/administration/projects/projects.component.ts
+++ Premedia.Applications.Imaging.Dashboard.Web/ClientApp/src/app/modules/base/administration/projects/projects.component.ts
@@ -14,28 +14,19 @@ import { ListBoxComponent } from '@syncfusion/ej2-angular-dropdowns';
 export class ProjectsComponent extends DestroySubscriptionsComponent{
   public data: { [key: string]: Object }[] = [];
   public newInputValue: string = '';
-
-  //@ViewChild('listbox') listbox!: ListBoxComponent;
-
   btnclick() {
     if (this.newInputValue.trim() !== '') {
       const newItems = this.newInputValue
-        .split(',') // Split input into individual items using commas as the separator
-        .map(item => item.trim()) // Trim whitespace from each item
-        .filter(item => item !== ''); // Remove empty items
+        .split(/\r?\n/)
+        .map(item => item.trim())
+        .filter(item => item !== '');
 
       this.data.push(...newItems.map(text => ({ text })));
       this.newInputValue = '';
     }
-   /*if(this.listbox){
-    let addItem: { [key: string]: Object }[] = [
-      { text: this.newInputValue}
-    ];
-      this.listbox.addItems(addItem);
-   }
-   console.log(this.newInputValue);
-   this.newInputValue = '';
-   */
+
+    console.log(this.data);
+
   }
 }
 
